fc7e931e52112745f99ca83b580f62a2604745f5
Rajneshlodhi <rajneshlodhi19@gmail.com>
2025-05-22 20:59:10 +0530

    update

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export  const assets ={
     parcel_icon
 }
 export default assets;
-export const url = 'http://localhost:4000'
+export const url = 'https://food-delivery-backend-vl6c.onrender.com'
