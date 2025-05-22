633cb7c7833ee408f8c0a61160f1c7140c9b79d2
Rajneshlodhi <rajneshlodhi19@gmail.com>
2025-05-22 21:32:32 +0530

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeholder = async (req, res) => {
 
    
-    const frontend_url = process.env.FRONTEND_URL || "http://localhost:5173";
+    const frontend_url = process.env.FRONTEND_URL || "https://food-delivery-frontend-1ezb.onrender.com/";
 
 
     try {
